c137afa48d6c6a20c17e1d91c667f7185e087d02
Nethmi Chethana <nethmichethana656@gmail.com>
2026-08-22 19:55:13 +0530

    MediFind Reservation Controller created

--- src/main/java/com/ijse/MediFind/controller/ReservationController.java
+++ src/main/java/com/ijse/MediFind/controller/ReservationController.java
@@ -1,4 +1,18 @@
 package com.ijse.MediFind.controller;
 
+
+import com.ijse.MediFind.entity.Reservation;
+import com.ijse.MediFind.service.ReservationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
 public class ReservationController {
+
+    private final ReservationService reservationService;
+
+
 }
