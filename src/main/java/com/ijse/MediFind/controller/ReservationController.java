1deb114427883542b8ea1279fb1d1b2b272bb675
Nethmi Chethana <nethmichethana656@gmail.com>
2026-08-22 19:57:05 +0530

    MediFind Reservation Controller createReservation Controller Class Implememted

--- src/main/java/com/ijse/MediFind/controller/ReservationController.java
+++ src/main/java/com/ijse/MediFind/controller/ReservationController.java
@@ -1,11 +1,19 @@
 package com.ijse.MediFind.controller;
 
 
+import com.ijse.MediFind.constants.CommonResponse;
+import com.ijse.MediFind.dto.request.ReservationReqDTO;
+import com.ijse.MediFind.dto.response.ReservationResDTO;
 import com.ijse.MediFind.entity.Reservation;
 import com.ijse.MediFind.service.ReservationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static com.ijse.MediFind.constants.ResponseCode.OPERATION_SUCCESS;
+import static com.ijse.MediFind.constants.ResponseMessage.SUCCESS_MESSAGE;
 
 @RestController
 @RequestMapping
@@ -13,6 +21,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReservationController {
 
     private final ReservationService reservationService;
+
+    @PostMapping("/reservations")
+    public CommonResponse createReservation(
+            @RequestBody ReservationReqDTO reservationReqDTO) {
+
+        ReservationResDTO reservation =
+                reservationService.createReservation(
+                        reservationReqDTO
+                );
+
+        return new CommonResponse(
+                OPERATION_SUCCESS,
+                reservation,
+                SUCCESS_MESSAGE
+        );
+    }
 
 
 }
