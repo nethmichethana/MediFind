b24144d10c6da6d571e138e142dd01d8f5236ae3
Nethmi Chethana <nethmichethana656@gmail.com>
2026-08-22 10:58:44 +0530

    MediFind Reservation Repository Updated

--- src/main/java/com/ijse/MediFind/repository/ReservationRepository.java
+++ src/main/java/com/ijse/MediFind/repository/ReservationRepository.java
@@ -1,10 +1,11 @@
 package com.ijse.MediFind.repository;
 
 
+import com.ijse.MediFind.entity.Reservation;
 import com.ijse.MediFind.entity.ReservationItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ReservationRepository extends JpaRepository<ReservationItem, Integer> {
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 }
