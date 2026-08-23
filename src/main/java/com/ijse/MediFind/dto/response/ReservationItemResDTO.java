7a4812ec284036ba8cbf714357e2cf62e0ca7af8
Nethmi Chethana <nethmichethana656@gmail.com>
2026-08-23 09:56:06 +0530

    ResevationItemResDTO implemented

--- src/main/java/com/ijse/MediFind/dto/response/ReservationItemResDTO.java
+++ src/main/java/com/ijse/MediFind/dto/response/ReservationItemResDTO.java
@@ -1,4 +1,21 @@
 package com.ijse.MediFind.dto.response;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ReservationItemResDTO {
+
+    private Long id;
+
+    private Integer quantity;
+
+    private Double unitPrice;
+
+    private Long reservationId;
+
+    private Long medicineId;
 }
