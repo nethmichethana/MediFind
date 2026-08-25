609397748f200a96a30cb312c88a5b18a1668421
Nethmi Chethana <nethmichethana656@gmail.com>
2026-08-25 21:29:32 +0530

    AuditLog ServiceImpl Structure Created

--- src/main/java/com/ijse/MediFind/service/impl/AuditLogServiceImpl.java
+++ src/main/java/com/ijse/MediFind/service/impl/AuditLogServiceImpl.java
@@ -1,4 +1,20 @@
 package com.ijse.MediFind.service.impl;
 
-public class AuditLogServiceImpl {
+import com.ijse.MediFind.repository.AuditLogRepository;
+import com.ijse.MediFind.service.AuditLogService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class AuditLogServiceImpl  implements AuditLogService {
+
+    private final AuditLogRepository auditLogRepository;
+
+    @Override
+    public void log(Long userId, String action, String entityName, Long entityId, String description) {
+
+    }
 }
