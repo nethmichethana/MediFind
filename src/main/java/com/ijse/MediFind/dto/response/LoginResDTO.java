ff0fc52be1c5ed38d6cd9f7d349d6f18d9472730
Nethmi Chethana <nethmichethana656@gmail.com>
2026-08-26 23:37:29 +0530

    LoginResDTO Completed

--- src/main/java/com/ijse/MediFind/dto/response/LoginResDTO.java
+++ src/main/java/com/ijse/MediFind/dto/response/LoginResDTO.java
@@ -1,4 +1,21 @@
 package com.ijse.MediFind.dto.response;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class LoginResDTO {
+
+    private String token;
+
+    private Long userId;
+
+    private String name;
+
+    private String email;
+
+    private String role;
 }
