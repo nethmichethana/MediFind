479224f4e592695a4cd3157571c23d0182bb0b49
Nethmi Chethana <nethmichethana656@gmail.com>
2026-08-27 00:27:25 +0530

    AuthServiceImpl Completed

--- src/main/java/com/ijse/MediFind/service/impl/AuthServiceImpl.java
+++ src/main/java/com/ijse/MediFind/service/impl/AuthServiceImpl.java
@@ -2,12 +2,15 @@ package com.ijse.MediFind.service.impl;
 
 import com.ijse.MediFind.dto.request.LoginReqDTO;
 import com.ijse.MediFind.dto.response.LoginResDTO;
+import com.ijse.MediFind.dto.response.UserResDTO;
+import com.ijse.MediFind.entity.User;
 import com.ijse.MediFind.repository.UserRepository;
 import com.ijse.MediFind.security.JwtUtil;
 import com.ijse.MediFind.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -21,6 +24,39 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public LoginResDTO login(LoginReqDTO loginReqDTO) {
-        return null;
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginReqDTO.getEmail(),
+                        loginReqDTO.getPassword()
+                )
+        );
+
+        User user = userRepository
+                .findByEmail(loginReqDTO.getEmail())
+                .orElseThrow(() ->
+                        new RuntimeException(
+                                "User not found with email: "
+                                        + loginReqDTO.getEmail()
+                        )
+                );
+
+        UserResDTO userResDTO = UserResDTO.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .email(user.getEmail())
+                .phone(user.getPhone())
+                .status(user.getStatus())
+                .roleId(user.getRole().getId())
+                .build();
+
+        String token = jwtUtil.generateToken(userResDTO);
+
+        return LoginResDTO.builder()
+                .token(token)
+                .userId(user.getId())
+                .name(user.getName())
+                .email(user.getEmail())
+                .role(user.getRole().getRoleName().name())
+                .build();
     }
 }
