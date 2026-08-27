0c686ba17e0d9ce6ae076e4f2abb5c6ca889cda3
Nethmi Chethana <nethmichethana656@gmail.com>
2026-08-27 10:57:59 +0530

    AuthController Created

--- src/main/java/com/ijse/MediFind/controller/AuthController.java
+++ src/main/java/com/ijse/MediFind/controller/AuthController.java
@@ -1,4 +1,15 @@
 package com.ijse.MediFind.controller;
 
+import com.ijse.MediFind.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final AuthService authService;
+
 }
