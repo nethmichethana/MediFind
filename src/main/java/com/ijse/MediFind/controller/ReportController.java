b9d64d453d2a06cce2c4fd5a156b789ce174ebdd
Nethmi Chethana <nethmichethana656@gmail.com>
2026-08-30 18:36:18 +0530

    ReportController completed

--- src/main/java/com/ijse/MediFind/controller/ReportController.java
+++ src/main/java/com/ijse/MediFind/controller/ReportController.java
@@ -1,4 +1,14 @@
 package com.ijse.MediFind.controller;
 
+import com.ijse.MediFind.service.ReportService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
 public class ReportController {
+
+    private final ReportService reportService;
 }
