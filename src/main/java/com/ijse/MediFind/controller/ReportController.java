e7d34cf466f08f95c81485fba6bb50efdb6d1e57
Nethmi Chethana <nethmichethana656@gmail.com>
2026-08-30 18:37:12 +0530

    ReportController completed

--- src/main/java/com/ijse/MediFind/controller/ReportController.java
+++ src/main/java/com/ijse/MediFind/controller/ReportController.java
@@ -1,9 +1,16 @@
 package com.ijse.MediFind.controller;
 
+import com.ijse.MediFind.constants.CommonResponse;
 import com.ijse.MediFind.service.ReportService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+import static com.ijse.MediFind.constants.ResponseCode.OPERATION_SUCCESS;
+import static com.ijse.MediFind.constants.ResponseMessage.SUCCESS_MESSAGE;
 
 @RestController
 @RequestMapping
@@ -11,4 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReportController {
 
     private final ReportService reportService;
+
+    @GetMapping
+    public CommonResponse generateReport() {
+
+        Map<String, Object> report = reportService.generateReport();
+
+        return new CommonResponse(
+                OPERATION_SUCCESS,
+                report,
+                SUCCESS_MESSAGE
+        );
+    }
 }
